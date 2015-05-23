e1722ec666dfc7f4fb48fd4562e642c2aa81ff82
Samuel Jean <jamael.seun@gmail.com>
2015-05-23 07:32:47 -0400

    make room for others to contribute the project

--- ws/conn.go
+++ ws/conn.go
@@ -1,3 +1,7 @@
+// Copyright 2015 The GoPivo Authors. All rights reserved.
+// Use of this source code is governed by a Simplified BSD
+// license that can be found in the LICENSE file.
+
 package ws
 
 import (
