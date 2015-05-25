a4f723a6dd145b1af2720cd9f2c2d4b243e6ea5a
Samuel Jean <jamael.seun@gmail.com>
2015-05-25 15:26:31 -0400

    better start/stop methods

--- hub.go
+++ hub.go
@@ -19,7 +19,9 @@ const defaultJoinLimitRateBurst = 32
 const defaultJoinMaxQueueSize = 256
 
 var (
-	ErrHubKillWentBad   = errors.New("killing hub raised errors")
+	ErrHubFlushErrors   = errors.New("errors while flushing")
+	ErrHubIsNotRunning  = errors.New("hub is not running yet")
+	ErrHubIsRunning     = errors.New("hub is already running")
 	ErrJoinQueueIsFull  = errors.New("join queue is full")
 	ErrNoSuchConnector  = errors.New("no such connector")
 	ErrPortBufferIsFull = errors.New("port buffer is full")
@@ -52,22 +54,30 @@ type Broadcast struct {
 
 // A Hub is a collection of Connectors.
 type Hub struct {
-	lock     *sync.Mutex
-	ports    ports
-	queue    chan chan bool
-	throttle chan time.Time
+	lock    *sync.Mutex
+	ports   ports
+	qburst  uint
+	qrate   time.Duration
+	qsize   uint
+	qslot   chan bool
+	queue   chan chan bool
+	running bool
 }
 
 type ports map[Connector]chan []byte
 
+type throttler struct {
+	stop chan bool
+}
+
 func NewHub() *Hub {
 	h := &Hub{
-		lock:     &sync.Mutex{},
-		ports:    make(ports),
-		queue:    make(chan chan bool, defaultJoinMaxQueueSize),
-		throttle: make(chan time.Time, defaultJoinLimitRateBurst),
+		lock:   &sync.Mutex{},
+		ports:  make(ports),
+		qburst: defaultJoinLimitRateBurst,
+		qsize:  defaultJoinMaxQueueSize,
 	}
-	go h.run()
+	h.qrate = time.Second / defaultJoinLimitRatePerSecond
 	return h
 }
 
@@ -91,17 +101,36 @@ func (b *Broadcast) Close() {
 }
 
 func (h *Hub) run() {
-	go h.ticker(defaultJoinLimitRatePerSecond)
+	h.running = true
+	throttler := h.throttler(h.qrate)
+	defer func() {
+		h.running = false
+		throttler.stop <- true
+	}()
+
 	for waiter := range h.queue {
-		<-h.throttle
-		waiter <- true
+		waiter <- <-h.qslot
 	}
 }
 
-func (h *Hub) ticker(rate time.Duration) {
-	for ns := range time.Tick(time.Second / rate) {
-		h.throttle <- ns
-	}
+func (h *Hub) throttler(rate time.Duration) *throttler {
+	throttler := &throttler{stop: make(chan bool)}
+	go func() {
+		ticker := time.NewTicker(rate)
+		defer func() { ticker.Stop(); close(h.qslot) }()
+		for {
+			select {
+			case <-throttler.stop:
+				return
+			case <-ticker.C:
+				select {
+				case h.qslot <- true:
+				default:
+				}
+			}
+		}
+	}()
+	return throttler
 }
 
 func (h *Hub) waitQueue() error {
@@ -117,10 +146,20 @@ func (h *Hub) waitQueue() error {
 	return nil
 }
 
-func (h *Hub) NewBroadcast() *Broadcast {
-	bc := &Broadcast{C: make(chan []byte)}
-	go bc.broadcast(h)
-	return bc
+func (h *Hub) Flush(reason error) (error, []error) {
+	h.lock.Lock()
+	defer h.lock.Unlock()
+	var errors []error
+	for c := range h.ports {
+		if err := c.Closer(reason); err != nil {
+			errors = append(errors, err)
+		}
+	}
+
+	if len(errors) > 0 {
+		return ErrHubFlushErrors, errors
+	}
+	return nil, nil
 }
 
 func (h *Hub) Join(c Connector, r io.ReadCloser, w Welcomer) error {
@@ -153,22 +192,6 @@ func (h *Hub) Join(c Connector, r io.ReadCloser, w Welcomer) error {
 	return nil
 }
 
-func (h *Hub) Kill(reason error) (error, []error) {
-	h.lock.Lock()
-	defer h.lock.Unlock()
-	var errors []error
-	for c := range h.ports {
-		if err := c.Closer(reason); err != nil {
-			errors = append(errors, err)
-		}
-	}
-
-	if len(errors) > 0 {
-		return ErrHubKillWentBad, errors
-	}
-	return nil, nil
-}
-
 func (h *Hub) Leave(c Connector, reason error) error {
 	h.lock.Lock()
 	defer h.lock.Unlock()
@@ -177,4 +200,28 @@ func (h *Hub) Leave(c Connector, reason error) error {
 		return c.Closer(reason)
 	}
 	return ErrNoSuchConnector
+}
+
+func (h *Hub) NewBroadcast() *Broadcast {
+	bc := &Broadcast{C: make(chan []byte)}
+	go bc.broadcast(h)
+	return bc
+}
+
+func (h *Hub) Start() error {
+	if h.running {
+		return ErrHubIsRunning
+	}
+	h.queue = make(chan chan bool, h.qsize)
+	h.qslot = make(chan bool, h.qburst)
+	go h.run()
+	return nil
+}
+
+func (h *Hub) Stop(reason error) (error, []error) {
+	if !h.running {
+		return ErrHubIsNotRunning, nil
+	}
+	close(h.queue)
+	return h.Flush(reason)
 }
